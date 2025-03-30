d293e27e21e07d710d6800ca29a698eb7b28f86c
TonilsonJM <tonilsonmonteiro2@gmail.com>
2025-03-30 16:59:15 +0100

    Delete aula.js

--- aula.js
+++
@@ -1,9 +0,0 @@
-
-var burguerbuttom  = document.querySelector(".burguerbox");
-var navcolaps = document.querySelector(".navcolaps");
-
-burguerbuttom.addEventListener("click", () => {
-
-    var isHidden = navcolaps.classList.contains("hidden");
-    isHidden ? navcolaps.classList.remove("hidden") : navcolaps.classList.add("hidden");
-})
